44e936569a50cf760cc10526aea129339ca27583
paul <harlandpauly@gmail.com>
2016-04-21 19:02:26 +0100

    now event type is dropdown

--- app/scripts/controllers/editors/eventeditor.js
+++ app/scripts/controllers/editors/eventeditor.js
@@ -10,26 +10,12 @@
 angular.module('adminApp')
   .controller('EventEditorCtrl', ['$scope', 'hopups', '$q', function ($scope, hopups, $q) {
 
-    this.eventsTypes = [
+        this.eventTypes = [
           {display: 'click', value: 'click', code: 'click'},
           {display: 'mouseover', value: 'mouseover', code: 'mouseover'},
           {display: 'initialpageload', value: 'initialpageload', code: 'initialpageload'},
           {display: 'eventfired', value: 'eventfired', code: 'eventfired'}
         ];
-
-        this.querySearch = function(query) {
-          var results = query ? this.eventsTypes.filter( this.createFilterFor(query) ) : [];
-          var deferred = $q.defer();
-          deferred.resolve( results );
-          return deferred.promise;
-        }
-
-        this.createFilterFor = function(query) {
-          var lowercaseQuery = angular.lowercase(query);
-          return function filterFn(state) {
-            return (state.value.indexOf(lowercaseQuery) === 0);
-          };
-        }
 
         this.add = function(site) {
           //we check if the dropdown has been used to change the event value
